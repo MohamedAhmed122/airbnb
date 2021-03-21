a2e97c27cae45e6c6661ef0e2723b44cd58e2700
MohamedAhmed122 <mohamed.ahm9833@gmail.com>
2021-03-21 15:12:09 +0700

    First Commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import {StyleSheet, StatusBar} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle={'dark-content'} />
+    </>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
