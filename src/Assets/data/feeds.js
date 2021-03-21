da7a509c2e2b76af0de2c3cd0bf175adc620ce79
MohamedAhmed122 <mohamed.ahm9833@gmail.com>
2021-03-21 16:46:36 +0700

    Create the Rent Detail Screen

--- src/Assets/data/feeds.js
+++ src/Assets/data/feeds.js
@@ -3,7 +3,8 @@ export default [
     id: '0',
     image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg',
     type: 'Private Room',
-    title: 'Bright room in the heart of the city',
+    title:
+      'Bright room in the heart of the city Bright room in the heart of the city',
     bed: 2,
     bedroom: 3,
     oldPrice: 25,
@@ -18,7 +19,8 @@ export default [
     id: '1',
     image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/2.jpg',
     type: 'Entire Flat',
-    title: 'NEW lux. apartment in the center of Santa Cruz',
+    title:
+      'NEW lux. apartment in the center of Santa Cruz NEW lux. apartment in the center of Santa Cruz',
     bed: 3,
     bedroom: 2,
     oldPrice: 76,
@@ -33,7 +35,8 @@ export default [
     id: '2',
     image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/3.jpg',
     type: 'Private Property',
-    title: 'Green House Santa Cruz',
+    title:
+      'Green House Santa Cruz Green House Santa Cruz Green House Santa Cruz Green House Santa Cruz ',
     bed: 2,
     bedroom: 1,
     oldPrice: 64,
@@ -48,7 +51,8 @@ export default [
     id: '3',
     image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/4.jpg',
     type: 'Entire Flat',
-    title: 'Typical canarian house',
+    title:
+      'Typical canarian house Typical canarian house Typical canarian house Typical canarian house',
     bed: 4,
     bedroom: 3,
     oldPrice: 120,
--- src/Screen/RentScreenDetails/index.js
+++ src/Screen/RentScreenDetails/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Image, StyleSheet, Text, View} from 'react-native';
+import {gray, primary} from '../../Config/colors';
 
 export default function RentDetails({route}) {
   const item = route.params;
@@ -8,12 +9,48 @@ export default function RentDetails({route}) {
     <View>
       <Image
         source={{uri: item.image}}
-        resizeMethod="cover"
+        resizeMode="cover"
         style={styles.image}
       />
-      
+      <View style={styles.container}>
+        <Text style={styles.title}>{item.type}</Text>
+        <Text style={styles.price}>${item.totalPrice}</Text>
+      </View>
+      <Text style={styles.bedroom}>
+        {item.bed} Bed - {item.bedroom} Bedroom
+      </Text>
+      <Text style={styles.des}>{item.title}</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    width: '100%',
+    height: 400,
+  },
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginHorizontal: 20,
+    marginTop: 14,
+  },
+  title: {
+    fontSize: 20,
+  },
+  price: {
+    color: primary,
+    fontSize: 18,
+  },
+  bedroom: {
+    color: gray,
+    fontSize: 17,
+    marginHorizontal: 20,
+    marginTop: 10,
+  },
+  des: {
+    marginTop: 15,
+    fontSize: 17,
+    textAlign: 'center',
+  },
+});
