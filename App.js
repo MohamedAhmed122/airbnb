6ce0e379d61e06ff9bebf47aa3d4489ba0a14068
MohamedAhmed122 <mohamed.ahm9833@gmail.com>
2021-03-22 10:53:19 +0700

    Conifg Redux

--- App.js
+++ App.js
@@ -1,14 +1,8 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import {StyleSheet, StatusBar} from 'react-native';
-import HomeScreen from './src/Screen/HomeScreen';
-import RentScreen from './src/Screen/RentScreen';
-import SearchScreen from './src/Screen/SearchScreen';
+import {StyleSheet} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
 import TabNavigation from './src/Navigation/TabNavigation';
-
-const Stack = createStackNavigator();
 
 const App = () => {
   return (
